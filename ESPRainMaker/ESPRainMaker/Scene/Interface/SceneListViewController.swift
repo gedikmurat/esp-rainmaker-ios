2ad1e25b5c3d02c023a0c62c892339128fd6e989
Oztug Evsizler <76663743+oztug@users.noreply.github.com>
2022-04-27 02:20:32 +0300

    Update SceneListViewController.swift

--- ESPRainMaker/ESPRainMaker/Scene/Interface/SceneListViewController.swift
+++ ESPRainMaker/ESPRainMaker/Scene/Interface/SceneListViewController.swift
@@ -44,7 +44,7 @@ class SceneListViewController: UIViewController {
         
         //hide nav bar
         navigationController?.navigationBar.isHidden = true
-        editButton.setTitleColor(UIColor(hexString: "#456078"), for: .normal)
+        editButton.setTitleColor(.white, for: .normal)
         
         //setup table view UI
         setupTable()
